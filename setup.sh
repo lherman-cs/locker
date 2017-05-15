06e57e650b119622a5f8abe0f01abd9532e1666c
Lukas Herman <lukashh6@gmail.com>
2017-05-14 23:47:43 -0400

    Fix broken setup script

--- setup.sh
+++ setup.sh
@@ -5,4 +5,5 @@ DEPENDENCIES=(build-essential libssl-dev libffi-dev python3-dev)
 
 sudo apt-get install ${DEPENDENCIES[@]}
 virtualenv -p $(which python3) env
+source env/bin/activate
 pip3 install -r requirements.txt
